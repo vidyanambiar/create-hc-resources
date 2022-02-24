62f971e7e3ae5e56d1116a5149d097c4b2586ed6
Vidya Nambiar <vnambiar@redhat.com>
2022-02-24 11:48:59 -0500

    Initial setup for lambda and cdk deployment

--- README.md
+++ README.md
@@ -1,33 +1,27 @@
 # Create resources in AWS for a hosted cluster in Hypershift
 
-This code can be used to create the infra and IAM resources in AWS that are required for creating a hosted cluster in hypershift. The output JSONs contain the ARNs that can be used to create a hosted cluster using the `hypershift create cluster` command.
+This repo deploys a Go-based AWS Lambda function using AWS CDK. The Lambda function is used to create the infra and IAM resources in AWS that are required for creating a hosted cluster in hypershift. 
+The output JSON contains the ARNs that can be used to create a hosted cluster using the `hypershift create cluster` command.
 
 The documentation here describes the resources that get created for infra and IAM: 
 https://hypershift-docs.netlify.app/how-to/create-infra-iam-separately/
 
-##### Environment variables to set before running the code:
+#### Lambda
+Implemented as a GO Lambda function - The code is located in the `/hc-resources-lambda` folder.
+
+#### CDK deployment
+The code for the CDK stack to deploy the lambda is located in the `/hc-resources-cdk` folder
+#### Inputs for the Lambda event:
 
 ```bash
-# Region where cluster infra should be created
-export REGION=...
-# A name for the hosted cluster
-export CLUSTER_NAME=...
-# Infrastructure ID to use for AWS resources.
-export INFRA_ID=...
-# Path to an AWS credentials file
-export AWS_CREDS=...
-# The ingress base domain for the cluster
-export BASE_DOMAIN=...
-# The name of the bucket in which the OIDC discovery document is stored
-export OIDC_BUCKET_NAME=...
-# The region of the bucket in which the OIDC discovery document is stored
-export OIDC_BUCKET_REGION=...
-```
-
-```
-// build
-go build
-
-// run go binary
-create-hc-resources
+{
+  "region": "value",            // Region where cluster infra should be created
+  "infraID": "value",           // Infrastructure ID to use for AWS resources
+  "awsAccessKeyID": "value",    // AWS Access Key ID for account to create resources in
+  "awsSecretKey": "value",      // AWS Secret Key for account to create resources in
+  "name": "value",              // A name for the hosted cluster
+  "baseDomain": "value",        // The ingress base domain for the cluster
+  "oidcBucketName": "value",    // The name of the bucket in which the OIDC discovery document is stored
+  "oidcBucketRegion": "value",  // The region of the bucket in which the OIDC discovery document is stored
+}
 ```
---
+++ hc-resources-lambda/main.go
@@ -0,0 +1,172 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	cmd "github.com/openshift/hypershift/cmd/infra/aws"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type CreateResourcesEvent struct {
+	Region             string	`json:"region"`				// Region where cluster infra should be created
+	InfraID            string	`json:"infraID"`			// Infrastructure ID to use for AWS resources
+	AWSKey             string	`json:"awsAccessKeyID"`		// AWS Access Key ID for account to create resources in
+	AWSSecretKey       string	`json:"awsSecretKey"`		// AWS Secret Key for account to create resources in
+	Name               string	`json:"name"`				// A name for the hosted cluster
+	BaseDomain         string	`json:"baseDomain"`			// The ingress base domain for the cluster
+	OIDCStorageProviderS3BucketName string	`json:"oidcBucketName"`		// The name of the bucket in which the OIDC discovery document is stored
+	OIDCStorageProviderS3Region     string	`json:"oidcBucketRegion"`	// The region of the bucket in which the OIDC discovery document is stored
+}
+
+// The response is made up of outputs from the AWS infra and IAM hypershift commands
+type ResourcesResponse struct {
+	InfraOutput *cmd.CreateInfraOutput	`json:"infraOutput"`
+	IAMOutput *cmd.CreateIAMOutput		`json:"iamOutput"`
+}
+
+var createInfraOutput *cmd.CreateInfraOutput
+
+// Create AWS infra resources for a hosted cluster
+func createInfraResources(createResourcesEvent CreateResourcesEvent) (*cmd.CreateInfraOutput, error) {
+	log.Info("***** Create AWS infrastructure resources for a cluster")
+
+	createInfraOpts := cmd.CreateInfraOptions{
+		Region: 			createResourcesEvent.Region,		
+		Name:   			createResourcesEvent.Name,
+		InfraID:			createResourcesEvent.InfraID,
+		AWSKey:				createResourcesEvent.AWSKey,
+		AWSSecretKey:		createResourcesEvent.AWSSecretKey,
+		AWSCredentialsFile:	"",
+		BaseDomain:			createResourcesEvent.BaseDomain,
+	}
+
+	log.WithFields(log.Fields{
+		"Region":         	createInfraOpts.Region,
+		"Name":             createInfraOpts.Name,
+		"InfraID":          createInfraOpts.InfraID,
+		"BaseDomain":      	createInfraOpts.BaseDomain,
+	}).Info("Configuration Values for Infra:")	
+
+	var err error
+	
+	createInfraOutput, err = createInfraOpts.CreateInfra(context.TODO())
+
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Fatal("Error creating infra!")
+		return nil, err
+	} else {
+		log.WithFields(log.Fields{"result": createInfraOutput}).Info("Infra Output JSON")
+		return createInfraOutput, nil
+	}
+}
+
+// Create AWS IAM resources for a hosted cluster
+func createIAMResources(createResourcesEvent CreateResourcesEvent) (*cmd.CreateIAMOutput, error) {
+	// Get values for IAM options from environment variables
+	region := os.Getenv("REGION")
+	oidcBucketName := os.Getenv("OIDC_BUCKET_NAME")
+	oidcBucketRegion := os.Getenv("OIDC_BUCKET_REGION")
+	infraID := os.Getenv("INFRA_ID")
+	awsAccessKeyID := os.Getenv("AWS_ACCESS_KEY_ID")
+	awsSecretKey := os.Getenv("AWS_SECRET_KEY")
+	publicZoneID := createInfraOutput.PublicZoneID
+	privateZoneID := createInfraOutput.PrivateZoneID
+	localZoneID := createInfraOutput.LocalZoneID
+
+	log.Info("***** Create AWS IAM resources")
+
+	createIAMOpts := cmd.CreateIAMOptions{
+		Region: 							region,				// Region where cluster infra should be created
+		OIDCStorageProviderS3BucketName:	oidcBucketName,		// The name of the bucket in which the OIDC discovery document is stored
+		OIDCStorageProviderS3Region: 		oidcBucketRegion,	// The region of the bucket in which the OIDC discovery document is stored
+		InfraID:							infraID,			// Infrastructure ID to use for AWS resources. It is used to identify the IAM resources associated with the hosted cluster.
+		AWSCredentialsFile:					"",
+		AWSKey:								awsAccessKeyID,
+		AWSSecretKey:						awsSecretKey,	
+		PublicZoneID:						publicZoneID,		// The id of the cluster's public route53 zone
+		PrivateZoneID:						privateZoneID,		// The id of the cluster's private route53 zone
+		LocalZoneID: 						localZoneID,		// The id of the cluster's local route53 zone
+	}
+
+	log.WithFields(log.Fields{
+		"Region":         						createIAMOpts.Region,
+		"InfraID":								createIAMOpts.InfraID,
+		"OIDCStorageProviderS3BucketName":      createIAMOpts.OIDCStorageProviderS3BucketName,
+		"OIDCStorageProviderS3Region":          createIAMOpts.OIDCStorageProviderS3Region,																	
+		"PublicZoneID":      					createIAMOpts.PublicZoneID,
+		"PrivateZoneID":						createIAMOpts.PrivateZoneID,
+		"LocalZoneID":							createIAMOpts.LocalZoneID,
+	}).Info("Configuration Values for IAM:")
+
+	result, err := createIAMOpts.CreateIAM(context.TODO(), nil)
+
+	if err != nil {
+		log.WithFields(log.Fields{"error": err}).Fatal("Error creating IAM!")
+		return nil, err
+	} else {
+		log.WithFields(log.Fields{"result": result}).Info("IAM Output JSON")
+		return result, nil
+	}	
+}
+
+// Lambda event handler
+func HandleRequest(ctx context.Context, createResourcesEvent CreateResourcesEvent) (ResourcesResponse, error) {
+	// Validate event attributes
+	if (createResourcesEvent.AWSKey == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing AWS access key")
+	}
+	
+	if (createResourcesEvent.AWSSecretKey == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing AWS secret Key")
+	}
+	
+	if (createResourcesEvent.InfraID == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing infraID")
+	}
+
+	if (createResourcesEvent.Name == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing cluster name")
+	}	
+
+	if (createResourcesEvent.BaseDomain == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing baseDomain")
+	}
+
+	if (createResourcesEvent.OIDCStorageProviderS3BucketName == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing oidcBucketName")
+	}
+
+	if (createResourcesEvent.OIDCStorageProviderS3Region == "") {
+		return ResourcesResponse{}, fmt.Errorf("missing oidcBucketRegion")
+	}	
+	
+	// Note: infra must be created first as values from the infra output will be used for creating IAM resources
+	createInfraOutput, err := createInfraResources(createResourcesEvent)
+
+	outputResponse := ResourcesResponse{}
+
+	if err != nil {
+		return outputResponse, err
+	} else {
+		outputResponse.InfraOutput = createInfraOutput
+	}
+
+	createIAMOutput, err := createIAMResources(createResourcesEvent)
+
+	if err != nil {
+		return outputResponse, err
+	} else {
+		outputResponse.IAMOutput = createIAMOutput
+	}
+	
+	return outputResponse, nil
+}
+
+func main() {
+	// Execute the Lambda function
+	lambda.Start(HandleRequest)
+}
